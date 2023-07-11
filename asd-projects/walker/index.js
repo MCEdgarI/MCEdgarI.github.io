cb0ddbbcaca9e0eb99253977979adf2e026a0cc8
MCEdgarI <decpboss@gmail.com>
2023-07-11 14:17:29 +0000

    walker fixed

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -28,8 +28,8 @@ function runProgram(){
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('keyDown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
-  $(document).on('keyUp' , handleKeyUp);
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup' , handleKeyUp);
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
